6fd2c3825334629ddc02366b348b5434d5d850a2
Muhammad-Momin <160271670+Muhammad-Momin@users.noreply.github.com>
2024-04-26 16:21:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const ans = await inquirer.prompt({
+    name: "q1",
+    type: "input",
+    message: "ENter your sentence to count words"
+});
+console.log(ans.q1);
+let done = ans.q1.trim().split(" ");
+console.log(done);
+console.log(`your word count is ${done.length}`);
